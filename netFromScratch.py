0606a6f546b98b8e144014ab89125af7de1057b7
oba2311 <oba2311@gmail.com>
2018-10-05 20:22:19 +0100

    first commit for the assignment - siraj's example

--- netFromScratch.py
+++ netFromScratch.py
@@ -1,2 +1,18 @@
-print('hey')
- 
+# numpy used for mathematical purposes:
+import numpy as np
+# matplotlib used for plotting:
+import matplotlib.pyplot as plt
+
+# activation func:
+def sigmoid(x):
+    activationResult = 1/(1+np.e**(-x))
+    return activationResult
+
+def sigmoid_derivative(x):
+    sigmoid(x) * (1- sigmoid(x))
+
+# Plot Activation Results to understand the
+# Sigmoids behavior:
+plottingData = np.linspace(-10,10, num=100)
+plt.plot(sigmoid(plottingData))
+plt.show()
